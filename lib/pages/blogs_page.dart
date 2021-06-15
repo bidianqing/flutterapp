0a4c5de95f2ec9731850edc534308b8b96ba62e2
毕殿卿 <bdq123@outlook.com>
2021-06-15 23:10:37 +0800

    temp

---
+++ lib/pages/blogs_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class BlogsPage extends StatefulWidget {
+  const BlogsPage({Key? key}) : super(key: key);
+
+  @override
+  _BlogsPageState createState() => _BlogsPageState();
+}
+
+class _BlogsPageState extends State<BlogsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Blogs Page'),
+      ),
+      backgroundColor: Color.fromARGB(0, 246, 246, 246),
+      body: ListView.builder(
+          itemCount: 50,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+                child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Text(
+                    'Title of Blog ${index + 1}',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                      'Free. Cross-platform. Open source.A framework for building web apps and services with .NET and C#.'),
+                ],
+              ),
+            ));
+          }),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/pages/account_page.dart';
+import 'package:myapp/pages/blogs_page.dart';
+import 'package:myapp/pages/favorites_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  PageController _pageController = PageController();
+  List<Widget> _screens = [BlogsPage(), FavoritesPage(), AccountPage()];
+
+  int _currentIndex = 0;
+  void _onPageChanged(int index) {}
+
+  void _itemTapped(int index) {
+    _pageController.jumpToPage(index);
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        children: _screens,
+        onPageChanged: _onPageChanged,
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _itemTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Blogs',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Account',
+          )
+        ],
+      ),
+    );
+  }
+}
